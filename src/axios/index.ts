20231a3f1c0a99e6ee3c99e5a2f5d64040d422da
iliabes <oksana14888@gmail.com>
2022-06-18 15:44:33 +0500

    temporarily

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL:process.env.REACT_APP_API_URL
+    baseURL:'https://api.openweathermap.org/data/2.5'
 })
 
 
 api.interceptors.request.use(function (config) {
-    config.url = config.url + '&lang=ru' + '&units=metric' + '&appid=' + process.env.REACT_APP_API_KEY
+    config.url = config.url + '&lang=ru' + '&units=metric' + '&appid=' + '2607112734128ccb8b24ee86e9d33c4f'
     return config;
 });
